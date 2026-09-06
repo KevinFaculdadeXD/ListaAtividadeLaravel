4984d54c24d534244b8344681f4110d72a3f9244
KevinFaculdadeXD <engs-kevinrichardt@camporeal.edu.br>
2026-09-06 15:44:18 -0300

    ATV 1: Crie as rotas '/sobre', '/alunos' e '/contato', que devem apenas retornar um texto;

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre', function () {
+    return 'Pagina sobre sla oq kkkkk';
+});
+
+Route::get('/alunos', function () {
+    return 'Pagina sobre alunos muitos foda';
+});
+
+Route::get('/contato', function () {
+    return 'meu contato ai 42 6969-6767';
+});
