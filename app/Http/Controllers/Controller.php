d29d09395f4ffaf6ff3a2cd31c8f7b364583252d
KevinFaculdadeXD <engs-kevinrichardt@camporeal.edu.br>
2026-09-06 20:47:56 -0300

    ATV 22: Crie uma Policy para proteger ações sobre um determinado registro; ATV 23: Ex: Deixe apenas Admin cadastrar e excluir Aluno, já Professor pode editar;

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,32 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function create()
+{
+    $this->authorize('create', Aluno::class);
+    return view('alunos.create');
+}
+
+public function store(AlunoRequest $request)
+{
+    $this->authorize('create', Aluno::class);
+    Aluno::create($request->validated());
+    return redirect()->route('alunos.index');
+}
+
+public function update(AlunoRequest $request, $id)
+{
+    $aluno = Aluno::findOrFail($id);
+    $this->authorize('update', $aluno);
+    $aluno->update($request->validated());
+    return redirect()->route('alunos.show', $aluno->id);
+}
+
+public function destroy($id)
+{
+    $aluno = Aluno::findOrFail($id);
+    $this->authorize('delete', $aluno);
+    Aluno::destroy($id);
+    return redirect()->route('alunos.index');
+}
 }
