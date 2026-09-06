424e430f758b87b22aa22e8e52b40071cabb3f3d
KevinFaculdadeXD <engs-kevinrichardt@camporeal.edu.br>
2026-09-06 20:11:34 -0300

    ATV 18: Adicione o Breeze no Projeto;

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sobre', function () {
-    return 'Pagina sobre sla oq kkkkk';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/alunos', function () {
-    return 'Pagina sobre alunos muitos foda';
-});
-
-Route::get('/contato', function () {
-    return 'meu contato ai 42 6969-6767';
-});
-
-Route::get('/produto/{id}', function ($id) {
-    return "Produto daora número: $id";
-});
-
-Route::get('/categoria/{id}', function ($id) {
-    return "Categoria número: $id";
-});
-
-Route::get('/usuario/{id}', function ($id) {
-    return "Usuário foda número: $id";
-});
-
-use App\Http\Controllers\AlunoController;
-
-Route::resource('alunos', AlunoController::class);
+require __DIR__.'/auth.php';
