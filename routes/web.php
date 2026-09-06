15effed5653c869b05713632254bfb3696132f01
KevinFaculdadeXD <engs-kevinrichardt@camporeal.edu.br>
2026-09-06 20:45:25 -0300

    ATV 21: Crie um Middleware para impedir acesso indevido em algumas rotas: Ex: '/admin' ou '/professor';

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/admin', function () {
+    return 'Painel administrativo';})->middleware('admin');
